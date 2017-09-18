878613beb3637982f79ce9265c711dea6a625d0b
Ashu Gupta <ash.gupta2406@gmail.com>
2017-09-18 14:11:39 +0530

    Added untimed async executor, used for example, kafka consumer etc.

---
+++ auth/src/main/java/co/shipz/auth/config/ExecutorBeans.java
@@ -0,0 +1,15 @@
+package co.shipz.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Component
+public class ExecutorBeans {
+  @Bean(name = "kafkaExecutor")
+  public Executor kafkaExecutor() {
+    return Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
+  }
+}
